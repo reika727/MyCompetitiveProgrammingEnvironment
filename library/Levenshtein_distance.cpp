848e77b4c8f50744e176b9bf72873c664f588067
reika727 <reikasuya727@gmail.com>
2023-10-17 16:30:50 +0900

    fix Levenshtein_distance.cpp

--- library/Levenshtein_distance.cpp
+++ library/Levenshtein_distance.cpp
@@ -1,5 +1,5 @@
 /*
-https://atcoder.jp/contests/pastbook2022/submissions/46681190
+https://atcoder.jp/contests/pastbook2022/submissions/46681659
 */
 
 template <
@@ -25,23 +25,23 @@ std::size_t Levenshtein_distance(
 )
 {
     const auto m = std::ranges::size(r1), n = std::ranges::size(r2);
-    if (m > n) {
+    if (m < n) {
         return Levenshtein_distance(std::forward<R2>(r2), std::forward<R1>(r1), pred, proj2, proj1);
     }
-    const auto distance = std::make_unique<std::size_t[]>(m + 1);
-    std::iota(distance.get(), distance.get() + (m + 1), 0uz);
-    for (auto j = 0uz; j < n; ++j) {
-        auto prev = std::exchange(distance[0], j + 1);
-        for (auto i = 0uz; i < m; ++i){
+    const auto distance = std::make_unique<std::size_t[]>(n + 1);
+    std::iota(distance.get(), distance.get() + (n + 1), 0uz);
+    for (auto i = 0uz; i < m; ++i) {
+        auto prev = std::exchange(distance[0], i + 1);
+        for (auto j = 0uz; j < n; ++j) {
             prev = std::exchange(
-                distance[i + 1],
+                distance[j + 1],
                 std::min({
-                    distance[i + 1] + 1,
-                    distance[i] + 1,
+                    distance[j + 1] + 1,
+                    distance[j] + 1,
                     prev + (pred(proj1(r1[i]), proj2(r2[j])) ? 0 : 1)
                 })
             );
         }
     }
-    return distance[m];
+    return distance[n];
 }
