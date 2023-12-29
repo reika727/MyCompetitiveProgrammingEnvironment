f151db25cf00879ea8c10940adb1dd6702bf8a90
reika727 <reikasuya727@gmail.com>
2023-12-30 00:52:38 +0900

    Fix get-samples.ts

--- atcoder-utils/get-samples.ts
+++ atcoder-utils/get-samples.ts
@@ -1,46 +1,21 @@
 import fs from 'fs'
 import jsdom from 'jsdom'
 
-const fetch_retry_after = async (ms: number, url: string, options = {}) => {
-  while (true) {
-    const response = await fetch(url, options)
-    if (response.status == 429) {
-      await new Promise(_ => setTimeout(_, ms))
-    } else {
-      return response
-    }
-  }
-}
-
 const [ cookiePath, contestId ] = process.argv.slice(2)
 
 const cookieJar = await jsdom.CookieJar.fromJSON(fs.readFileSync(cookiePath, 'utf8'))
 
 const downloadTasks =
-  await jsdom.JSDOM.fromURL(`https://atcoder.jp/contests/${contestId}/tasks`, { cookieJar })
+  await jsdom.JSDOM.fromURL(`https://atcoder.jp/contests/${contestId}/tasks_print`, { cookieJar })
   .then(
     dom => Array.from(
-      dom.window.document.querySelectorAll('tr > td:first-child > a'),
-      a => (a as HTMLAnchorElement).href
+      dom.window.document.querySelectorAll('.col-sm-12:not(.next-page):not(.alert)')
     )
   )
   .then(
-    taskURLs => taskURLs.map(
-      async (taskURL) => {
-        const text =
-          await fetch_retry_after(
-            500,
-            taskURL,
-            {
-              headers: {
-                Cookie: cookieJar.getCookieStringSync('https://atcoder.jp')
-              }
-            }
-          )
-          .then(response => response.text())
-
-        const nodes = new jsdom.JSDOM(text).window.document.querySelectorAll('#task-statement > .lang > .lang-ja h3+pre')
-        /* see https://img.atcoder.jp/public/8cd1ac6/js/contest.js : 63 */
+    col_sm_12s => col_sm_12s.map(
+      async (col_sm_12) => {
+        const nodes = col_sm_12.querySelectorAll('#task-statement > .lang > .lang-ja h3+pre')
 
         const samples: { input: string; output: string }[] = []
         for (let i = 0; i < nodes.length; i += 2) {
@@ -50,7 +25,7 @@ const downloadTasks =
           })
         }
 
-        const problemId = /_(.+)$/.exec(taskURL)?.[1]
+        const problemId = /^(.+) -/.exec(col_sm_12.querySelector('.h2')!.textContent!)?.[1]
         return [ problemId, samples ]
       }
     )
