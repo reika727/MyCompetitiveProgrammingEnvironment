be26b468c6ca880227ed1153c10e4e7018e0df33
reika727 <reikasuya727@gmail.com>
2023-12-30 22:32:55 +0900

    Refactor .ts's

--- atcoder-utils/get-samples.ts
+++ atcoder-utils/get-samples.ts
@@ -1,37 +1,40 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import jsdom from 'jsdom'
 
 const [ cookiePath, contestId ] = process.argv.slice(2)
 
-const cookieJar = await jsdom.CookieJar.fromJSON(fs.readFileSync(cookiePath, 'utf8'))
-
-const downloadTasks =
-  await jsdom.JSDOM.fromURL(`https://atcoder.jp/contests/${contestId}/tasks_print`, { cookieJar })
-  .then(
-    dom => Array.from(
-      dom.window.document.querySelectorAll('.col-sm-12:not(.next-page):not(.alert)')
-    )
+await fs.readFile(cookiePath, 'utf8')
+.then(
+  json => jsdom.JSDOM.fromURL(
+    `https://atcoder.jp/contests/${contestId}/tasks_print`,
+    { cookieJar: jsdom.CookieJar.fromJSON(json) }
   )
-  .then(
-    col_sm_12s => col_sm_12s.map(
-      async col_sm_12 => {
-        const nodes = col_sm_12.querySelectorAll('#task-statement > .lang > .lang-ja h3+pre')
+)
+.then(
+  dom => Array.from(
+    dom.window.document.querySelectorAll('.col-sm-12:not(.next-page):not(.alert)')
+  )
+)
+.then(
+  col_sm_12s => col_sm_12s.map(
+    async col_sm_12 => {
+      const nodes = col_sm_12.querySelectorAll('#task-statement > .lang > .lang-ja h3+pre')
 
-        const samples: { input: string; output: string }[] = []
-        for (let i = 0; i < nodes.length; i += 2) {
-          samples.push({
-            input: nodes[i]?.textContent ?? '',
-            output: nodes[i + 1]?.textContent ?? ''
-          })
-        }
-
-        const problemId = /^(.+) -/.exec(col_sm_12.querySelector('.h2')!.textContent!)?.[1]
-        return [ problemId, samples ]
+      const samples: { input: string; output: string }[] = []
+      for (let i = 0; i < nodes.length; i += 2) {
+        samples.push({
+          input: nodes[i]?.textContent ?? '',
+          output: nodes[i + 1]?.textContent ?? ''
+        })
       }
-    )
-  )
 
-await Promise.all(downloadTasks)
-.then(samples => {
+      const problemId = /^(.+) -/.exec(col_sm_12.querySelector('.h2')!.textContent!)?.[1]
+
+      return [ problemId, samples ]
+    }
+  )
+)
+.then(downloadTasks => Promise.all(downloadTasks))
+.then(samples =>
   console.log(JSON.stringify(Object.fromEntries(samples), null, 2))
-})
+)
--- atcoder-utils/login.ts
+++ atcoder-utils/login.ts
@@ -3,8 +3,10 @@ import tough from 'tough-cookie'
 async function getLoginCookieJar(userName: string, password: string) {
   const url = 'https://atcoder.jp/login'
 
-  const temporaryCookies = await fetch(url)
+  const temporaryCookies =
+    await fetch(url)
     .then(response => response.headers.getSetCookie())
+
   const csrfToken = /csrf_token:(.+?=)/.exec(decodeURIComponent(temporaryCookies[1]))?.[1]!
 
   const body = new FormData
@@ -12,7 +14,8 @@ async function getLoginCookieJar(userName: string, password: string) {
   body.set('password', password)
   body.set('csrf_token', csrfToken)
 
-  const setCookie = fetch(url, {
+  const cookieJar = new tough.CookieJar()
+  await fetch(url, {
     method: 'POST',
     redirect: 'manual',
     headers: new Headers(
@@ -21,14 +24,16 @@ async function getLoginCookieJar(userName: string, password: string) {
       )
     ),
     body
-  }).then(response => response.headers.getSetCookie())
+  })
+  .then(response => response.headers.getSetCookie())
+  .then(setCookie => setCookie.forEach(
+      cookie => cookieJar.setCookie(cookie, url)
+    )
+  )
 
-  const cookieJar = new tough.CookieJar()
-  for (const cookie of await setCookie) {
-    cookieJar.setCookie(cookie, url)
-  }
-
-  const revelFlash = decodeURIComponent(cookieJar.getCookiesSync(url)[0].value)
+  const revelFlash =
+    await cookieJar.getCookies(url)
+    .then(cookies => decodeURIComponent(cookies[0].value))
 
   if (revelFlash == `\0success:Welcome,+${userName}.\0`) {
     return cookieJar
