b4f06acd9008cccf6cdda4aa3219ce04f087679a
reika727 <reikasuya727@gmail.com>
2024-01-16 02:10:54 +0900

    Check if problem id exists

--- test-samples.sh
+++ test-samples.sh
@@ -10,24 +10,29 @@ EXE='compro.out'
 make "$EXE"
 
 wrong_cases=0
-while read -r TEST_CASE; do
-        INPUT=$(echo "$TEST_CASE" | jq --raw-output '.input')
-        EXPECTED_OUTPUT=$(echo "$TEST_CASE" | jq --raw-output '.output')
-        OUTPUT=$(echo "$INPUT" | ./"$EXE")
-        if [ "$OUTPUT" == "$EXPECTED_OUTPUT" ]; then
-                echo -e '\e[32;1mTEST CASE PASSED!\e[m'
-        else
-                ((++wrong_cases))
-                echo ----------------
-                echo 'input:'
-                echo "$INPUT" | pr --omit-header --indent=2
-                echo 'expected output:'
-                echo "$EXPECTED_OUTPUT" | pr --omit-header --indent=2
-                echo 'output:'
-                echo "$OUTPUT" | pr --omit-header --indent=2
-                echo ----------------
-        fi
-done <<< "$(jq --compact-output --raw-output ."$PROBLEM_ID"[] "$JSON_NAME")"
+if TEST_CASES=$(jq --compact-output --raw-output ."$PROBLEM_ID"[] "$JSON_NAME"); then
+        while read -r TEST_CASE; do
+                INPUT=$(echo "$TEST_CASE" | jq --raw-output '.input')
+                EXPECTED_OUTPUT=$(echo "$TEST_CASE" | jq --raw-output '.output')
+                OUTPUT=$(echo "$INPUT" | ./"$EXE")
+                if [ "$OUTPUT" == "$EXPECTED_OUTPUT" ]; then
+                        echo -e '\e[32;1mTEST CASE PASSED!\e[m'
+                else
+                        ((++wrong_cases))
+                        echo ----------------
+                        echo 'input:'
+                        echo "$INPUT" | pr --omit-header --indent=2
+                        echo 'expected output:'
+                        echo "$EXPECTED_OUTPUT" | pr --omit-header --indent=2
+                        echo 'output:'
+                        echo "$OUTPUT" | pr --omit-header --indent=2
+                        echo ----------------
+                fi
+        done <<< "$TEST_CASES"
+else
+        echo "Problem ID '$PROBLEM_ID' does not exist."
+        exit 1
+fi
 
 if [ "$wrong_cases" != 0 ]; then
         echo -e "\e[31;1m$wrong_cases case(s) failed.\e[m"
