ec62805a82c4e1c50cab63cebcd7e1042def45db
reika727 <reikasuya727@gmail.com>
2024-02-03 10:20:42 +0900

    Update imos.cpp

--- library/imos.cpp
+++ library/imos.cpp
@@ -1,11 +1,11 @@
 /*
-https://atcoder.jp/contests/abc338/submissions/49850308
-https://yukicoder.me/submissions/947472
+https://atcoder.jp/contests/abc338/submissions/49897575
+https://yukicoder.me/submissions/947611
 */
 
 template <class T, size_t Dimension>
-class cyclic_imos final : private std::vector<cyclic_imos<T, Dimension - 1>> {
-    friend cyclic_imos<T, Dimension + 1>;
+class imos : protected std::vector<imos<T, Dimension - 1>> {
+    friend imos<T, Dimension + 1>;
 
 public:
     using coordinate = std::array<std::size_t, Dimension>;
@@ -16,13 +16,7 @@ public:
 
     constexpr inline static std::size_t dimension = Dimension;
 
-    using std::vector<cyclic_imos<T, Dimension - 1>>::begin;
-    using std::vector<cyclic_imos<T, Dimension - 1>>::end;
-    using std::vector<cyclic_imos<T, Dimension - 1>>::operator[];
-
-private:
-    using std::vector<cyclic_imos<T, Dimension - 1>>::size;
-
+protected:
     T& get(const std::span<const std::size_t, Dimension> idxs)
     {
         return (*this)[idxs.front()].get(idxs.template last<Dimension - 1>());
@@ -46,20 +40,24 @@ private:
         );
     }
 
-    cyclic_imos<T, Dimension> &operator+=(const cyclic_imos<T, Dimension> &ci)
+    imos<T, Dimension> &operator+=(const imos<T, Dimension> &ci)
     {
-        for (std::size_t i = 0; i < size(); ++i) {
+        for (std::size_t i = 0; i < this->size(); ++i) {
             (*this)[i] += ci[i];
         }
         return *this;
     }
 
 public:
+    using std::vector<imos<T, Dimension - 1>>::begin;
+    using std::vector<imos<T, Dimension - 1>>::end;
+    using std::vector<imos<T, Dimension - 1>>::operator[];
+
     template <class... Sizes>
-    cyclic_imos(const std::size_t size, const Sizes... sizes)
-        : std::vector<cyclic_imos<T, Dimension - 1>>(
+    imos(const std::size_t size, const Sizes... sizes)
+        : std::vector<imos<T, Dimension - 1>>(
               size + 1,
-              cyclic_imos<T, sizeof...(Sizes)>(sizes...)
+              imos<T, sizeof...(Sizes)>(sizes...)
           )
     {
     }
@@ -74,32 +72,9 @@ public:
         set(std::span{s.begin}, std::span{s.end}, weight);
     }
 
-    void set_cyclic(const segment &s, const T weight)
-    {
-        set(s, weight);
-        for (unsigned i = 1; i < (1 << Dimension); ++i) {
-            segment correction;
-            for (unsigned j = 0; j < Dimension; ++j) {
-                if (i & (1 << j)) {
-                    if (s.begin[j] <= s.end[j]) {
-                        goto CONTINUE;
-                    }
-                    correction.begin[j] = 0;
-                    correction.end[j] = size() - 1;
-                } else {
-                    correction.begin[j] = s.begin[j];
-                    correction.end[j] = s.end[j];
-                }
-            }
-            set(correction, weight);
-        CONTINUE:
-            ;
-        }
-    }
-
     void accumulate()
     {
-        for (std::size_t i = 0; i < size() - 1; ++i) {
+        for (std::size_t i = 0; i < this->size() - 1; ++i) {
             (*this)[i + 1] += (*this)[i];
         }
         for (auto &child : *this) {
@@ -109,8 +84,8 @@ public:
 };
 
 template <class T>
-class cyclic_imos<T, 1> final : private std::vector<T> {
-    friend cyclic_imos<T, 2>;
+class imos<T, 1> : protected std::vector<T> {
+    friend imos<T, 2>;
 
 public:
     using coordinate = std::array<std::size_t, 1>;
@@ -121,12 +96,7 @@ public:
 
     constexpr inline static std::size_t dimension = 1;
 
-    using std::vector<T>::begin;
-    using std::vector<T>::end;
-    using std::vector<T>::operator[];
-
 private:
-    using std::vector<T>::size;
     T& get(const std::span<const std::size_t, 1> idxs)
     {
         return (*this)[idxs.front()];
@@ -142,16 +112,20 @@ private:
         (*this)[end.front()] -= weight;
     }
 
-    cyclic_imos<T, 1> &operator+=(const cyclic_imos<T, 1> &ci)
+    imos<T, 1> &operator+=(const imos<T, 1> &ci)
     {
-        for (std::size_t i = 0; i < size(); ++i) {
+        for (std::size_t i = 0; i < this->size(); ++i) {
             (*this)[i] += ci[i];
         }
         return *this;
     }
 
 public:
-    cyclic_imos(const std::size_t size)
+    using std::vector<T>::begin;
+    using std::vector<T>::end;
+    using std::vector<T>::operator[];
+
+    explicit imos(const std::size_t size)
         : std::vector<T>(size + 1)
     {
     }
@@ -176,9 +150,53 @@ public:
         set({.begin = begin, .end = end}, weight);
     }
 
+    void accumulate()
+    {
+        for (std::size_t i = 0; i < this->size() - 1; ++i) {
+            (*this)[i + 1] += (*this)[i];
+        }
+    }
+};
+
+template <class T, size_t Dimension>
+class cyclic_imos final : public imos<T, Dimension> {
+public:
+    using typename imos<T, Dimension>::segment;
+    using imos<T, Dimension>::imos;
+
     void set_cyclic(const segment &s, const T weight)
     {
-        set(s, weight);
+        this->set(s, weight);
+        for (unsigned i = 1; i < (1 << Dimension); ++i) {
+            segment correction;
+            for (unsigned j = 0; j < Dimension; ++j) {
+                if (i & (1 << j)) {
+                    if (s.begin[j] <= s.end[j]) {
+                        goto CONTINUE;
+                    }
+                    correction.begin[j] = 0;
+                    correction.end[j] = this->size() - 1;
+                } else {
+                    correction.begin[j] = s.begin[j];
+                    correction.end[j] = s.end[j];
+                }
+            }
+            this->set(correction, weight);
+        CONTINUE:
+            ;
+        }
+    }
+};
+
+template <class T>
+class cyclic_imos<T, 1> final : public imos<T, 1> {
+public:
+    using typename imos<T, 1>::segment;
+    using imos<T, 1>::imos;
+
+    void set_cyclic(const segment &s, const T weight)
+    {
+        this->set(s, weight);
         if (s.begin.front() > s.end.front()) {
             (*this).front() += weight;
             (*this).back() -= weight;
@@ -189,14 +207,13 @@ public:
     {
         set_cyclic({.begin = begin, .end = end}, weight);
     }
-
-    void accumulate()
-    {
-        for (std::size_t i = 0; i < size() - 1; ++i) {
-            (*this)[i + 1] += (*this)[i];
-        }
-    }
 };
+
+template <class T>
+auto make_imos(auto... sizes)
+{
+    return imos<T, sizeof...(sizes)>(sizes...);
+}
 
 template <class T>
 auto make_cyclic_imos(auto... sizes)
