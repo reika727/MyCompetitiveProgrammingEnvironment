aa9488e2086d2bbc02cd4a8f8972820f56782f51
reika727 <reikasuya727@gmail.com>
2024-02-26 21:14:59 +0900

    Update prime_factorizer.cpp

--- library/prime_factorizer.cpp
+++ library/prime_factorizer.cpp
@@ -1,38 +1,72 @@
 /*
-https://atcoder.jp/contests/abc342/submissions/50642672
+https://atcoder.jp/contests/abc342/submissions/50661111
+https://atcoder.jp/contests/abc284/submissions/50661415
 */
 
-class prime_factorizer final
-{
+class prime_factorizer final {
 private:
-    std::vector<bool> is_prime_offset2;
-    std::vector<std::size_t> primes;
+    std::vector<std::size_t> primes{2};
 
-    void expand(const std::size_t n)
+    class primality_logger final {
+    private:
+        std::vector<bool> log;
+
+    public:
+        void expand(const std::size_t n)
+        {
+            if (n > max()) {
+                log.resize((n - 1) / 2, true);
+            }
+        }
+
+        void exclude(const std::size_t n)
+        {
+            if (n > 2 && n % 2 != 0) {
+                expand(n);
+                log[n / 2 - 1] = false;
+            }
+        }
+
+        std::size_t max()
+        {
+            return log.size() * 2 + 2;
+        }
+
+        bool check(const std::size_t n)
+        {
+            return n == 2 || (n > 2 && n % 2 != 0 && log[n / 2 - 1]);
+        }
+    } primalities;
+
+    void sieve_of_Eratosthenes(const std::size_t n)
     {
-        const auto previous_max = is_prime_offset2.size() + 1;
+        const auto previous_max = primalities.max();
         if (n <= previous_max) {
             return;
         }
-        is_prime_offset2.resize(n - 1, true);
-        for (const auto prime : primes) {
+        primalities.expand(n);
+        for (const auto prime : primes | std::views::drop(1) /* HACK */) {
             if (prime > n / prime) {
                 break;
             }
-            for (
-                auto i = std::max(previous_max / prime + 1, prime);
-                i <= n / prime;
-                ++i
-            ) {
-                is_prime_offset2[i * prime - 2] = false;
+            for (const auto i : std::views::iota(std::max(previous_max / prime + 1, prime))) {
+                if (i > n / prime) {
+                    break;
+                }
+                primalities.exclude(i * prime);
             }
         }
-        for (auto prime = previous_max + 1; prime <= n; ++prime) {
-            if (is_prime_offset2[prime - 2]) {
-                primes.push_back(prime);
-                for (auto i = prime; i <= n / prime; ++i) {
-                    is_prime_offset2[i * prime - 2] = false;
+        for (
+            const auto prime :
+            std::views::iota(previous_max + 1, n + 1) |
+            std::views::filter([this](const auto i){return primalities.check(i);})
+        ) {
+            primes.push_back(prime);
+            for (const auto i : std::views::iota(prime)) {
+                if (i > n / prime) {
+                    break;
                 }
+                primalities.exclude(i * prime);
             }
         }
     }
@@ -42,19 +76,24 @@ public:
 
     prime_factorizer(const std::size_t n)
     {
-        expand(n);
+        sieve_of_Eratosthenes(n);
     }
 
-    std::span<const std::size_t> get_primes(const std::size_t n = 0)
+    std::span<const std::size_t> get_known_primes()
     {
-        expand(n);
+        return primes;
+    }
+
+    std::span<const std::size_t> get_primes(const std::size_t n)
+    {
+        sieve_of_Eratosthenes(n);
         return primes;
     }
 
     bool is_prime(const std::size_t n)
     {
-        expand(n);
-        return n >= 2 && is_prime_offset2[n - 2];
+        sieve_of_Eratosthenes(n);
+        return primalities.check(n);
     }
 
     struct factor final {
@@ -72,11 +111,11 @@ public:
                 factors.emplace_back(n, 1);
                 break;
             }
-            if (n % prime == 0) {
-                std::size_t exponent = 0;
-                for (; n % prime == 0; n /= prime) {
-                    ++exponent;
-                }
+            std::size_t exponent = 0;
+            for (; n % prime == 0; n /= prime) {
+                ++exponent;
+            }
+            if (exponent != 0) {
                 factors.emplace_back(prime, exponent);
             }
         }
