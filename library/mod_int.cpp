fec2454b7a5b7dc882cb7cdbe4f7909934ebe78a
reika727 <reikasuya727@gmail.com>
2024-02-29 10:52:28 +0900

    Update mod_int.cpp

--- library/mod_int.cpp
+++ library/mod_int.cpp
@@ -1,16 +1,24 @@
 /*
-https://atcoder.jp/contests/abc162/submissions/48622618
-https://atcoder.jp/contests/abc145/submissions/48622645
-https://atcoder.jp/contests/abc333/submissions/48622686
+https://atcoder.jp/contests/abc162/submissions/50714576
+https://atcoder.jp/contests/abc145/submissions/50714592
+https://atcoder.jp/contests/abc333/submissions/50714598
 */
-template <unsigned long long mod>
-class basic_mod_int {
+
+template <std::unsigned_integral auto Mod>
+class basic_mod_int final {
+public:
+    using integer_type = decltype(Mod);
+
 private:
-    inline static std::map<unsigned long long, basic_mod_int> cache;
-    unsigned long long num;
+    inline static std::map<integer_type, basic_mod_int> inverse_cache;
+    constexpr inline static integer_type max = std::numeric_limits<integer_type>::max();
+    integer_type num;
 
 public:
-    static basic_mod_int pow(basic_mod_int x, unsigned long long y)
+    constexpr inline static integer_type mod = Mod;
+
+    template <std::unsigned_integral T>
+    static basic_mod_int pow(basic_mod_int x, T y)
     {
         basic_mod_int ans = 1;
         for (; y; x *= x, y >>= 1) {
@@ -20,79 +28,123 @@ public:
         }
         return ans;
     }
+
     basic_mod_int() = default;
-    basic_mod_int(unsigned long long n)
+
+    basic_mod_int(integer_type n)
         : num(n % mod)
     {
     }
-    explicit operator unsigned long long() const
+
+    integer_type value() const
     {
         return num;
     }
+
     bool operator==(const basic_mod_int &mi) const
     {
         return num == mi.num;
     }
+
     bool operator!=(const basic_mod_int &mi) const
     {
         return !operator==(mi);
     }
+
     basic_mod_int operator~() const
     {
-        if (cache.contains(num)) {
-            return cache[num];
+        if (inverse_cache.contains(num)) {
+            return inverse_cache[num];
         } else {
-            return cache[num] = pow(*this, mod - 2);
+            return inverse_cache[num] = pow(*this, mod - 2);
         }
     }
+
     basic_mod_int operator+(const basic_mod_int &mi) const
     {
-        return num + mi.num;
+        if constexpr (mod - 1 <= max - (mod - 1)) {
+            return num + mi.num;
+        } else {
+            if (num <= mod - mi.num) {
+                return num + mi.num;
+            } else {
+                return num - (mod - mi.num);
+            }
+        }
     }
+
     basic_mod_int operator-(const basic_mod_int &mi) const
     {
-        return num + mod - mi.num;
+        return operator+(mod - mi.num);
     }
+
     basic_mod_int operator*(const basic_mod_int &mi) const
     {
-        return num * mi.num;
+        if constexpr (mod - 1 <= max / (mod - 1)) {
+            return num * mi.num;
+        } else {
+            if (num <= mod / mi.num) {
+                return num * mi.num;
+            } else {
+                basic_mod_int mult = 1;
+                auto a = *this;
+                auto b = mi.num;
+                for (; b; b >>= 1, a += a) {
+                    if (b & 1) {
+                        mult += a;
+                    }
+                }
+                return mult;
+            }
+        }
     }
+
     basic_mod_int operator/(const basic_mod_int &mi) const
     {
-        return num * (~mi).num;
+        return operator*(~mi);
     }
+
     basic_mod_int &operator+=(const basic_mod_int &mi)
     {
         return *this = operator+(mi);
     }
+
     basic_mod_int &operator-=(const basic_mod_int &mi)
     {
         return *this = operator-(mi);
     }
+
     basic_mod_int &operator*=(const basic_mod_int &mi)
     {
         return *this = operator*(mi);
     }
+
     basic_mod_int &operator/=(const basic_mod_int &mi)
     {
         return *this = operator/(mi);
     }
 };
-template <unsigned long long mod>
-class combination_modulo final : basic_mod_int<mod> {
-    using mod_int = basic_mod_int<mod>;
 
+template <std::unsigned_integral auto Mod>
+class combination_modulo final {
 private:
+    using mod_int = basic_mod_int<Mod>;
+
     std::vector<mod_int> factorial_mod;
 
+    using size_type = decltype(factorial_mod)::size_type;
+
 public:
-    combination_modulo(unsigned long long max_n)
+    combination_modulo(const size_type max_n)
         : factorial_mod(max_n + 1)
     {
         factorial_mod[0] = 1;
-        for (unsigned long long i = 1; i <= max_n; ++i) factorial_mod[i] = factorial_mod[i - 1] * i;
+        for (size_type i = 1; i <= max_n; ++i) {
+            factorial_mod[i] = factorial_mod[i - 1] * i;
+        }
     }
-    mod_int operator()(unsigned long long n, unsigned long long r)
+
+    mod_int operator()(const size_type n, const size_type r)
     {
         return n < r ? 0 : factorial_mod[n] / (factorial_mod[r] * factorial_mod[n - r]);
     }
