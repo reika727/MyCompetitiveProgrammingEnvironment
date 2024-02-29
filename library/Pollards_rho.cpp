268f640e913d8525476b1da17ac25c47b9aa8a8a
reika727 <reikasuya727@gmail.com>
2024-02-29 20:38:09 +0900

    Update Pollards_rho.cpp

--- library/Pollards_rho.cpp
+++ library/Pollards_rho.cpp
@@ -1,9 +1,9 @@
 /*
-https://atcoder.jp/contests/abc284/submissions/50713480
+https://atcoder.jp/contests/abc284/submissions/50724473
 */
 
-template <std::unsigned_integral T>
-T mod_addition(T a, T b, T mod)
+template <std::unsigned_integral UInt>
+UInt mod_addition(UInt a, UInt b, UInt mod)
 {
     a %= mod;
     b %= mod;
@@ -14,10 +14,10 @@ T mod_addition(T a, T b, T mod)
     }
 };
 
-template <std::unsigned_integral T>
-T mod_multiply(T a, T b, T mod)
+template <std::unsigned_integral UInt>
+UInt mod_multiply(UInt a, UInt b, UInt mod)
 {
-    T ans = 0;
+    UInt ans = 0;
     for (; b; b >>= 1, a = mod_addition(a, a, mod)) {
         if (b & 1) {
             ans = mod_addition(ans, a, mod);
@@ -26,10 +26,10 @@ T mod_multiply(T a, T b, T mod)
     return ans;
 }
 
-template <std::unsigned_integral T>
-T mod_pow(T a, T b, T mod)
+template <std::unsigned_integral UInt>
+UInt mod_pow(UInt a, UInt b, UInt mod)
 {
-    T ans = 1;
+    UInt ans = 1;
     for (; b; b >>= 1, a = mod_multiply(a, a, mod)) {
         if (b & 1) {
             ans = mod_multiply(ans, a, mod);
@@ -40,9 +40,9 @@ T mod_pow(T a, T b, T mod)
 
 class Miller_Rabin final {
 private:
-    template <std::unsigned_integral T>
+    template <std::unsigned_integral UInt>
     static bool is_possibly_prime(
-        const T n,
+        const UInt n,
         std::ranges::input_range auto &&witnesses
     )
     {
@@ -54,7 +54,7 @@ private:
         const auto m = n - 1;
         const auto s = std::countr_zero(m);
         const auto d = m >> s;
-        for (const T a : witnesses) {
+        for (const UInt a : witnesses) {
             if (a >= n) {
                 continue;
             }
@@ -62,7 +62,7 @@ private:
             if (x == 1) {
                 continue;
             }
-            for (auto r = 0; x != m; ++r, x = mod_pow(x, static_cast<T>(2), n)) {
+            for (auto r = 0; x != m; ++r, x = mod_pow(x, static_cast<UInt>(2), n)) {
                 if (r == s - 1 || x == 1) {
                     return false;
                 }
@@ -73,20 +73,19 @@ private:
 
 public:
     template <
-        std::unsigned_integral T,
-        std::unsigned_integral U,
+        std::unsigned_integral UInt,
         std::uniform_random_bit_generator Generator = std::mt19937
     >
     static bool test(
-        const T n,
-        const U repitition,
+        const UInt n,
+        const std::unsigned_integral auto repitition,
         const Generator::result_type seed = std::random_device{}()
     )
     {
         /* TODO: use std::generator after C++23 */
         Generator engine(seed);
-        std::uniform_int_distribution<T> dist(2, n - 1);
-        std::vector<T> random_numbers;
+        std::uniform_int_distribution<UInt> dist(2, n - 1);
+        std::vector<UInt> random_numbers;
         random_numbers.reserve(repitition);
         while (random_numbers.size() < repitition) {
             random_numbers.push_back(dist(engine));
@@ -94,11 +93,11 @@ public:
         return is_possibly_prime(n, random_numbers);
     }
 
-    template <std::unsigned_integral T>
-    static bool test_uint64(const T n)
+    template <std::unsigned_integral UInt>
+    static bool test_uint64(const UInt n)
     {
-        static_assert(std::numeric_limits<T>::digits <= 64);
-        constexpr char witnesses[] = {
+        static_assert(std::numeric_limits<UInt>::digits <= 64);
+        constexpr unsigned char witnesses[] = {
             2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37
         };
         return is_possibly_prime(n, witnesses);
@@ -109,52 +108,78 @@ public:
 
 class Pollards_rho final {
 private:
-    template <std::unsigned_integral T>
-    static std::optional<T> search_nontrivial_factor(const T n)
+    template <std::unsigned_integral UInt, class Polynomial>
+        requires std::is_invocable_r_v<UInt, Polynomial, UInt>
+    static std::optional<UInt> search_nontrivial_factor(
+        const UInt n,
+        const Polynomial f
+    )
     {
-        if (n % 2 == 0) {
-            return 2;
+        for (UInt x = f(0), y = f(f(0)); ; x = f(x), y = f(f(y))) {
+            const auto g = std::gcd(std::max(x, y) - std::min(x, y), n);
+            if (g == 1) {
+                continue;
+            }
+            if (g == n) {
+                break;
+            }
+            return g;
         }
-        for (T c = 1; c < n; ++c) {
-            const auto f = [n, c](const T x) {
-                return mod_addition(mod_pow(x, static_cast<T>(2), n), c, n);
-            };
-            for (T x = f(0), y = f(f(0)); ; x = f(x), y = f(f(y))) {
-                const auto g = std::gcd(std::max(x, y) - std::min(x, y), n);
-                if (g == 1) {
-                    continue;
-                }
-                if (g == n) {
-                    break;
-                }
-                return g;
+        return std::nullopt;
+    }
+
+    template <std::unsigned_integral UInt, class Primality, class Polynomial>
+        requires std::predicate<Primality, UInt> &&
+                 std::is_invocable_r_v<UInt, Polynomial, UInt>
+    static std::optional<UInt> search_prime_factor(
+        const UInt n,
+        const Primality check_primality,
+        const Polynomial f
+    )
+    {
+        for (
+            std::optional factor = n;
+            factor;
+            factor = search_nontrivial_factor(*factor, f)
+       ) {
+            if (check_primality(*factor)) {
+                return *factor;
+            } else if (check_primality(n / *factor)) {
+                return n / *factor;
             }
         }
         return std::nullopt;
     }
 
-    template <std::unsigned_integral T, class P>
-        requires std::predicate<P, T>
-    static std::optional<T> search_prime_factor(const T n, const P check_primality)
+    template <std::unsigned_integral UInt, class Primality>
+        requires std::predicate<Primality, UInt>
+    static std::optional<UInt> search_prime_factor_with_quadratics(
+        const UInt n,
+        const Primality check_primality
+    )
     {
-        for (std::optional<T> f = n; f; f = search_nontrivial_factor(*f)) {
-            if (check_primality(*f)) {
-                return *f;
-            } else if (check_primality(n / *f)) {
-                return n / *f;
+        if (n % 2 == 0) {
+            return 2;
+        }
+        for (UInt c = 1; c < n; ++c) {
+            const auto f = [n, c](const UInt x) {
+                return mod_addition(mod_pow(x, static_cast<UInt>(2), n), c, n);
+            };
+            if (const auto p = search_prime_factor(n, check_primality, f)) {
+                return p;
             }
         }
         return std::nullopt;
     }
 
 public:
-    template <std::unsigned_integral T, class P>
-        requires std::predicate<P, T>
-    static std::map<T, T> factorize(T n, const P check_primality)
+    template <std::unsigned_integral UInt, class Primality>
+        requires std::predicate<Primality, UInt>
+    static std::map<UInt, UInt> factorize(UInt n, const Primality check_primality)
     {
-        std::map<T, T> factors;
+        std::map<UInt, UInt> factors;
         while (n > 1) {
-            const auto p = search_prime_factor(n, check_primality);
+            const auto p = search_prime_factor_with_quadratics(n, check_primality);
             if (!p) {
                 break;
             }
@@ -165,10 +190,10 @@ public:
         return factors;
     }
 
-    template <std::unsigned_integral T>
-    static std::map<T, T> factorize_uint64(const T n)
+    template <std::unsigned_integral UInt>
+    static std::map<UInt, UInt> factorize_uint64(const UInt n)
     {
-        return factorize(n, [](const auto n){return Miller_Rabin::test_uint64(n);});
+        return factorize(n, static_cast<bool(*)(UInt)>(Miller_Rabin::test_uint64));
     }
 
     Pollards_rho() = delete;
