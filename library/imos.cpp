f0c0c26ff7b35b002abed7e1a0bd3619c91a05b8
reika727 <reikasuya727@gmail.com>
2024-03-06 23:12:24 +0900

    Update imos.cpp

--- library/imos.cpp
+++ library/imos.cpp
@@ -1,52 +1,45 @@
 /*
-https://atcoder.jp/contests/abc338/submissions/50289524
-https://yukicoder.me/submissions/950877
+https://atcoder.jp/contests/abc338/submissions/50955486
+https://yukicoder.me/submissions/957942
 */
 
 template <std::default_initializable T, size_t Dimension>
-class imos final
-    : private std::conditional_t<
-        Dimension == 1,
-        std::vector<T>,
-        std::vector<imos<T, Dimension - 1>>
-      >
-{
+class imos final {
     static_assert(Dimension != 0, "Dimension must not be 0.");
     friend imos<T, Dimension + 1>;
 
 private:
-    using lower_dimension = std::conditional_t<
-        Dimension == 1,
-        T,
-        imos<T, Dimension - 1>
-    >;
+    std::vector<
+        std::conditional_t<
+            Dimension == 1,
+            T,
+            imos<T, Dimension - 1>
+        >
+    > lower_dimensions;
 
     std::size_t cast_index(const std::ptrdiff_t index) const noexcept
     {
-        const std::ptrdiff_t elem_count = this->size() - 1;
+        const std::ptrdiff_t elem_count = lower_dimensions.size() - 1;
         return (index % elem_count + elem_count) % elem_count;
     }
 
     std::ptrdiff_t floor_index(const std::ptrdiff_t index) const noexcept
     {
-        const std::ptrdiff_t elem_count = this->size() - 1;
+        const std::ptrdiff_t elem_count = lower_dimensions.size() - 1;
         return index >= 0 ? index / elem_count : (index + 1) / elem_count - 1;
     }
 
     imos<T, Dimension> &operator+=(const imos<T, Dimension> &im)
     {
-        for (auto i = 0uz; i < this->size(); ++i) {
-            (*this)[i] += im[i];
+        for (auto i = 0uz; i < lower_dimensions.size(); ++i) {
+            lower_dimensions[i] += im.lower_dimensions[i];
         }
         return *this;
     }
 
 public:
     imos(const std::size_t size, const auto... sizes)
-        : std::vector<lower_dimension>(
-              size + 1,
-              lower_dimension(sizes...)
-          )
+        : lower_dimensions(size + 1, {sizes...})
     {
         static_assert(
             1 + sizeof...(sizes) == Dimension,
@@ -61,9 +54,9 @@ public:
             "Number of indices must be equal to Dimension."
         );
         if constexpr (sizeof...(indices) == 0) {
-            return (*this)[cast_index(index)];
+            return lower_dimensions[cast_index(index)];
         } else {
-            return (*this)[cast_index(index)].at(indices...);
+            return lower_dimensions[cast_index(index)].at(indices...);
         }
     }
 
@@ -78,31 +71,31 @@ public:
         }
         const auto cross_count = floor_index(end.front()) - floor_index(begin.front());
         if constexpr (Dimension > 1) {
-            (*this)[cast_index(begin.front())].set(
+            lower_dimensions[cast_index(begin.front())].set(
                 begin.template subspan<1>(),
                 end.template subspan<1>(),
                 weight
             );
-            (*this)[cast_index(end.front())].set(
+            lower_dimensions[cast_index(end.front())].set(
                 begin.template subspan<1>(),
                 end.template subspan<1>(),
                 -weight
             );
-            this->front().set(
+            lower_dimensions.front().set(
                 begin.template subspan<1>(),
                 end.template subspan<1>(),
                 weight * cross_count
             );
-            this->back().set(
+            lower_dimensions.back().set(
                 begin.template subspan<1>(),
                 end.template subspan<1>(),
                 -weight * cross_count
             );
         } else {
-            (*this)[cast_index(begin.front())] += weight;
-            (*this)[cast_index(end.front())] -= weight;
-            this->front() += weight * cross_count;
-            this->back() -= weight * cross_count;
+            lower_dimensions[cast_index(begin.front())] += weight;
+            lower_dimensions[cast_index(end.front())] -= weight;
+            lower_dimensions.front() += weight * cross_count;
+            lower_dimensions.back() -= weight * cross_count;
         }
     }
 
@@ -126,12 +119,12 @@ public:
 
     void accumulate()
     {
-        for (auto i = 0uz; i < this->size() - 1; ++i) {
-            (*this)[i + 1] += (*this)[i];
+        for (auto i = 0uz; i < lower_dimensions.size() - 1; ++i) {
+            lower_dimensions[i + 1] += lower_dimensions[i];
         }
         if constexpr (Dimension > 1) {
-            for (auto &child : *this) {
-                child.accumulate();
+            for (auto &lower_dimension : lower_dimensions) {
+                lower_dimension.accumulate();
             }
         }
     }
