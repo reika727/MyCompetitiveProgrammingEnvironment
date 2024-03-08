608b244b8d447a50ccb9d91d937772730196d20e
reika727 <reikasuya727@gmail.com>
2024-03-09 04:36:19 +0900

    Update weighted_union_find.cpp

--- library/weighted_union_find.cpp
+++ library/weighted_union_find.cpp
@@ -1,32 +1,36 @@
 /*
-https://atcoder.jp/contests/abc087/submissions/38084502
+https://atcoder.jp/contests/abc087/submissions/51001873
 */
 
+template <std::unsigned_integral UInt>
 class weighted_union_find final {
 private:
-    std::vector<unsigned long long> par, sz, rank;
-    std::vector<long long> diff_weight;
-    unsigned long long cnt;
+    std::vector<UInt> par;
+    std::vector<std::size_t> sz, rank;
+    std::vector<std::ptrdiff_t> diff_weight;
+    std::size_t cnt;
 
 public:
-    weighted_union_find(unsigned long long n)
+    weighted_union_find(const std::size_t n)
         : par(n), sz(n, 1), rank(n, 0), diff_weight(n, 0), cnt(n)
     {
-        std::iota(par.begin(), par.end(), 0ull);
+        std::iota(par.begin(), par.end(), static_cast<UInt>(0));
     }
-    unsigned long long find(unsigned long long n)
+
+    UInt find(const UInt n)
     {
         if (par[n] == n) {
             return n;
         } else {
-            unsigned long long root_n = find(par[n]);
+            const auto root_n = find(par[n]);
             diff_weight[n] += diff_weight[par[n]];
             return par[n] = root_n;
         }
     }
-    void unite(unsigned long long x, unsigned long long y, long long w)
+
+    void unite(const UInt x, const UInt y, const std::ptrdiff_t w)
     {
-        long long root_x = find(x), root_y = find(y);
+        const auto root_x = find(x), root_y = find(y);
         if (root_x != root_y) {
             if (rank[root_x] < rank[root_y]) {
                 diff_weight[root_x] = -(w + weight(x) - weight(y));
@@ -36,29 +40,36 @@ public:
                 diff_weight[root_y] = w + weight(x) - weight(y);
                 par[root_y] = root_x;
                 sz[root_x] += sz[root_y];
-                if (rank[root_x] == rank[root_y]) ++rank[root_x];
+                if (rank[root_x] == rank[root_y]) {
+                    ++rank[root_x];
+                }
             }
             --cnt;
         }
     }
-    bool is_same(unsigned long long x, unsigned long long y)
+
+    bool is_same(const UInt x, const UInt y)
     {
         return find(x) == find(y);
     }
-    unsigned long long size(unsigned long long n)
+
+    std::size_t size(const UInt n)
     {
         return sz[find(n)];
     }
-    unsigned long long count() const
+
+    std::size_t count() const noexcept
     {
         return cnt;
     }
-    long long weight(unsigned long long n)
+
+    std::ptrdiff_t weight(const UInt n)
     {
         find(n);
         return diff_weight[n];
     }
-    long long difference(unsigned long long x, unsigned long long y)
+
+    std::ptrdiff_t difference(const UInt x, const UInt y)
     {
         return weight(y) - weight(x);
     }
