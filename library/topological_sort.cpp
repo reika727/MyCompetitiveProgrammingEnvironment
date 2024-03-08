e39bf1cad7425efe7bb085895f7cb7948e20279d
reika727 <reikasuya727@gmail.com>
2024-03-09 03:35:09 +0900

    Update topological_sort.cpp

--- library/topological_sort.cpp
+++ library/topological_sort.cpp
@@ -1,40 +1,51 @@
 /*
-https://atcoder.jp/contests/abc291/submissions/39274543
+https://atcoder.jp/contests/abc291/submissions/51001333
+https://atcoder.jp/contests/abc223/submissions/51001574
+https://atcoder.jp/contests/abc216/submissions/51001640
 */
 
-template<class ElemInputIterator, class RelInputIterator, class OutputIterator>
-bool topological_sort(
-    ElemInputIterator elem_begin, ElemInputIterator elem_end,
-    RelInputIterator rel_begin, RelInputIterator rel_end,
-    OutputIterator out)
+template <
+    std::ranges::input_range Elements,
+    std::ranges::input_range Relations,
+    std::weakly_incrementable Out
+>
+    requires std::indirectly_writable<Out, std::ranges::range_value_t<Elements>>
+std::optional<bool> topological_sort(Elements&& elements, Relations&& relations, Out out)
 {
-    using T = typename ElemInputIterator::value_type;
+    using T = std::ranges::range_value_t<Elements>;
     std::map<T, std::set<T>> dests_of, sources_of;
-    for (auto itr = rel_begin; itr != rel_end; ++itr) {
-        auto [s, d] = *itr;
+    for (const auto& [s, d] : relations) {
         dests_of[s].insert(d);
         sources_of[d].insert(s);
     }
     std::set<T> sources;
-    for (auto itr = elem_begin; itr != elem_end; ++itr) {
-        if (sources_of[*itr].empty()) {
-            sources.insert(*itr);
+    for (const auto& e : elements) {
+        if (!sources_of.contains(e)) {
+            sources.insert(e);
         }
     }
     bool is_unique = true;
     while (!sources.empty()) {
         is_unique &= (sources.size() == 1);
-        auto source = *sources.begin();
+        const auto source = *sources.begin();
         sources.erase(sources.begin());
         *out++ = source;
-        while (!dests_of[source].empty()) {
-            auto dest = *dests_of[source].begin();
+        while (dests_of.contains(source)) {
+            const auto dest = *dests_of[source].begin();
             dests_of[source].erase(dests_of[source].begin());
+            if (dests_of[source].empty()) {
+                dests_of.erase(source);
+            }
             sources_of[dest].erase(sources_of[dest].find(source));
             if (sources_of[dest].empty()) {
+                sources_of.erase(dest);
                 sources.insert(dest);
             }
         }
     }
-    return is_unique;
+    if (sources_of.empty() && dests_of.empty()) {
+        return is_unique;
+    } else {
+        return std::nullopt;
+    }
 }
