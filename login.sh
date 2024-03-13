9d1baccb782169101d30047dae8019362323f16f
reika727 <reikasuya727@gmail.com>
2024-03-13 23:03:42 +0900

    Update login.sh

--- login.sh
+++ login.sh
@@ -2,19 +2,21 @@
 
 if [ -f login.cookie.json ]; then
         echo 'already loged in to AtCoder.'
-else
-        read -rp 'User Name: ' USER_NAME
-        read -rsp 'Password: ' PASSWORD
-        echo
-        echo 'logging in to AtCoder...'
-        if JSON=$(node atcoder-utils/dist/login "$USER_NAME" "$PASSWORD"); then
-                DEFAULT_UMASK=$(umask -p)
-                umask 0077
-                echo "$JSON" > login.cookie.json
-                eval "$DEFAULT_UMASK"
-                echo 'successfully loged in to AtCoder.'
-        else
-                echo 'login failed.'
-                exit 1
-        fi
+        exit 0
 fi
+
+read -rp 'User Name: ' USER_NAME
+read -rsp 'Password: ' PASSWORD
+echo
+echo 'logging in to AtCoder...'
+
+if ! JSON=$(node atcoder-utils/dist/login "$USER_NAME" "$PASSWORD"); then
+        echo 'login failed.'
+        exit 1
+fi
+
+DEFAULT_UMASK=$(umask -p)
+umask 0077
+echo "$JSON" > login.cookie.json
+eval "$DEFAULT_UMASK"
+echo 'successfully loged in to AtCoder.'
