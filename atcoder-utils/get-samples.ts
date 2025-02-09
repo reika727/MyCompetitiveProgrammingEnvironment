21e175cce5d3a10950be066c541ead31f22d9ed9
reika727 <reikasuya727@gmail.com>
2025-02-09 12:09:14 +0900

    refactor: remove unnecessary `async`

--- atcoder-utils/get-samples.ts
+++ atcoder-utils/get-samples.ts
@@ -10,26 +10,24 @@ const dom = await jsdom.JSDOM.fromURL(
   { cookieJar: jsdom.CookieJar.fromJSON(json) }
 )
 
-const downloadTasks =
+const problems =
   dom.window.document
   .querySelectorAll('.col-sm-12:not(.next-page):not(.alert)')
   .values()
-  .map(
-    async col_sm_12 => {
-      const nodes = col_sm_12.querySelectorAll('#task-statement > .lang > .lang-ja h3+pre')
+  .map(col_sm_12 => {
+    const nodes = col_sm_12.querySelectorAll('#task-statement > .lang > .lang-ja h3+pre')
 
-      const samples: { input: string; output: string }[] = []
-      for (let i = 0; i < nodes.length; i += 2) {
-        samples.push({
-          input: nodes[i].textContent!,
-          output: nodes[i + 1].textContent!
-        })
-      }
-
-      const problemId = /^(.+) -/.exec(col_sm_12.getElementsByClassName('h2')[0]!.textContent!)![1]
-
-      return [ problemId, samples ]
+    const samples: { input: string; output: string }[] = []
+    for (let i = 0; i < nodes.length; i += 2) {
+      samples.push({
+        input: nodes[i].textContent!,
+        output: nodes[i + 1].textContent!
+      })
     }
-  )
 
-console.log(JSON.stringify(Object.fromEntries(await Promise.all(downloadTasks)), null, 2))
+    const problemId = /^(.+) -/.exec(col_sm_12.getElementsByClassName('h2')[0]!.textContent!)![1]
+
+    return [ problemId, samples ]
+  })
+
+console.log(JSON.stringify(Object.fromEntries(problems), null, 2))
