6759888d44b5ba1584e8528061542bc431c90bbe
reika727 <reikasuya727@gmail.com>
2025-02-09 10:49:11 +0900

    refactor: Remove getLoginCookieJar()

--- atcoder-utils/login.ts
+++ atcoder-utils/login.ts
@@ -1,55 +1,52 @@
 import { CookieJar } from 'jsdom'
 
-async function getLoginCookieJar(userName: string, password: string) {
-  const url = new URL('https://atcoder.jp/login')
+const [ userName, password ] = process.argv.slice(2)
 
-  const temporaryCookies =
-    await fetch(url)
-    .then(response => response.headers.getSetCookie())
+const url = new URL('https://atcoder.jp/login')
 
-  const csrfToken =
-    temporaryCookies
-    .values()
-    .map(temporaryCookie => /csrf_token:(.+?=)/.exec(decodeURIComponent(temporaryCookie))?.[1])
-    .find(value => value !== undefined)
-
-  if (csrfToken === undefined) {
-    throw new Error("csrf_token was not found.")
-  }
-
-  const body = new FormData
-  body.set('username', userName)
-  body.set('password', password)
-  body.set('csrf_token', csrfToken)
-
-  const cookieJar = new CookieJar()
-  await fetch(url, {
-    method: 'POST',
-    redirect: 'manual',
-    headers: new Headers(
-      temporaryCookies.map(
-        temporaryCookie => ['Cookie', temporaryCookie]
-      )
-    ),
-    body
-  })
+const temporaryCookies =
+  await fetch(url)
   .then(response => response.headers.getSetCookie())
-  .then(setCookie => setCookie.forEach(
-      cookie => cookieJar.setCookie(cookie, url)
-    )
-  )
 
-  const revelFlash =
-    await cookieJar.store
-    .findCookie(url.hostname, '/', 'REVEL_FLASH')
-    .then(cookie => decodeURIComponent(cookie.value))
+const csrfToken =
+  temporaryCookies
+  .values()
+  .map(temporaryCookie => /csrf_token:(.+?=)/.exec(decodeURIComponent(temporaryCookie))?.[1])
+  .find(value => value !== undefined)
 
-  if (revelFlash === `\0success:Welcome,+${userName}.\0`) {
-    return cookieJar
-  } else {
-    throw new Error('login failed.')
-  }
+if (csrfToken === undefined) {
+  throw new Error("csrf_token was not found.")
 }
 
-const [ userName, password ] = process.argv.slice(2)
-console.log(JSON.stringify(await getLoginCookieJar(userName, password), null, 2))
+const body = new FormData
+body.set('username', userName)
+body.set('password', password)
+body.set('csrf_token', csrfToken)
+
+const cookieJar = new CookieJar()
+await fetch(url, {
+  method: 'POST',
+  redirect: 'manual',
+  headers: new Headers(
+    temporaryCookies.map(
+      temporaryCookie => ['Cookie', temporaryCookie]
+    )
+  ),
+  body
+})
+.then(response => response.headers.getSetCookie())
+.then(setCookie => setCookie.forEach(
+    cookie => cookieJar.setCookie(cookie, url)
+  )
+)
+
+const revelFlash =
+  await cookieJar.store
+  .findCookie(url.hostname, '/', 'REVEL_FLASH')
+  .then(cookie => decodeURIComponent(cookie.value))
+
+if (revelFlash === `\0success:Welcome,+${userName}.\0`) {
+  console.log(JSON.stringify(cookieJar, null, 2))
+} else {
+  throw new Error('login failed.')
+}
